f1827dca9edad1e95b6e78e6be2865a2f1f627cf
gaoyangchun <gaoyangchun@gaoyangchuntekiMac-mini.local>
2014-01-15 14:08:20 +0800

    Initial Commit

---
+++ StarkBRGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StarkBRGame
+//
+//  Created by gaoyangchun on 14-1-15.
+//  Copyright (c) 2014年 斯塔克互动科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
