4e739a5d87831beae0862dd76b0572a781bcb1f0
starkYC <yangchun.gao@stark-corp.com>
2014-03-17 11:48:55 +0800

    Initial Commit

---
+++ ASIDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ASIDemo
+//
+//  Created by gaoyangchun on 14-3-17.
+//  Copyright (c) 2014年 斯塔克互动科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ASIDemoTests/ASIDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  ASIDemoTests.m
+//  ASIDemoTests
+//
+//  Created by gaoyangchun on 14-3-17.
+//  Copyright (c) 2014年 斯塔克互动科技有限公司. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ASIDemoTests : XCTestCase
+
+@end
+
+@implementation ASIDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
